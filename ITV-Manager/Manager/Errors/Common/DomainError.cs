3c7ada5cb576fc8a6fbba7acf1570ba2c6d00dcd
nicolasorteg <nicolasortegaf22@gmail.com>
2026-05-08 15:45:28 +0200

    add domainerror

---
+++ ITV-Manager/Manager/Errors/Common/DomainError.cs
@@ -0,0 +1,7 @@
+﻿namespace Manager.Errors.Common;
+
+/// <summary>
+/// Clase abstracta para todos los errores del sistema
+/// </summary>
+/// <param name="Mensaje">Mensaje de error</param>
+public abstract record DomainError(string Mensaje);
