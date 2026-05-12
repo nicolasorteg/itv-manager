6edf0bc25c556971d489f1c1e59cd540db020fd7
nicolasorteg <nicolasortegaf22@gmail.com>
2026-05-12 18:47:05 +0200

    interfaz validador con result

---
+++ ITV-Manager/Manager/Validators/CitaValidator.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFunctionalExtensions;
+using Manager.Errors.Common;
+using Manager.Models;
+using Manager.Validators.Common;
+
+
+namespace Manager.Validators;
+
+public class CitaValidator: IValidator<Cita> {
+    public Result<Cita, DomainError> Validar(Cita cita) {
+        throw new NotImplementedException();
+    }
+}
---
+++ ITV-Manager/Manager/Validators/Common/IValidator.cs
@@ -0,0 +1,17 @@
+﻿using CSharpFunctionalExtensions;
+using Manager.Errors.Common;
+
+namespace Manager.Validators.Common;
+
+/// <summary> Contrato para la validación de entidades </summary>
+/// <typeparam name="T">Entidad a validar</typeparam>
+public interface IValidator<T> {
+    
+    /// <summary> Valida una entidad </summary>
+    /// <param name="entity">Entidad a validar</param>
+    /// <returns>
+    /// - La entidad si la validación ha sido un éxito.
+    /// - Error de dominio si no.
+    /// </returns>
+    Result<T, DomainError> Validar(T entity);
+}
