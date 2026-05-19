38a73a9fe68ce29bf26b1ac0feb2ca1ee2a0c43c
nicolasorteg <nicolasortegaf22@gmail.com>
2026-05-19 13:51:29 +0200

    add json interface

---
+++ ITV-Manager/Manager/Storage/Json/ICitaJsonStorage.cs
@@ -0,0 +1,7 @@
+﻿using Manager.Models;
+using Manager.Storage.Common;
+
+namespace Manager.Storage.Json;
+
+/// <summary> Contrato para guardado y escritura de citas en formato JSON </summary>
+public interface ICitaJsonStorage : IStorage<Cita> { }
