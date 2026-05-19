f034a79f29900b92b00f13a071b54ab3b4914b0f
nicolasorteg <nicolasortegaf22@gmail.com>
2026-05-19 12:44:45 +0200

    add citacsvstorage

---
+++ ITV-Manager/Manager/Storage/Csv/CitaCsvStorage.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using CSharpFunctionalExtensions;
+using Manager.Config;
+using Manager.Errors.Common;
+using Manager.Models;
+using Serilog;
+
+namespace Manager.Storage.Csv;
+
+public class CitaCsvStorage : ICitaCsvStorage {
+    
+    private readonly ILogger _logger = Log.ForContext<CitaCsvStorage>();
+    private const string Cabecera = "Id;Matricula;Dni;Marca;Modelo;Cilindrada;Motor;FechaMatriculacion;FechaInspeccion;CreatedAt;UpdatedAt;IsDeleted;DeletedAt";
+    
+    // constructores
+    public CitaCsvStorage() : this(AppConfig.DataFolder) { }
+    public CitaCsvStorage(string dataFolder) {
+        _logger.Debug("Inicializando la clase CitaCsvStorage");
+        InitStorage(dataFolder); // data
+    }
+    
+    /// <inheritdoc cref="ICitaCsvStorage.Write" />
+    public Result<bool, DomainError> Write(IEnumerable<Cita> items, string path) {
+        
+    }
+    
+    
+    /// <inheritdoc cref="ICitaCsvStorage.Save" />
+    public Result<IEnumerable<Cita>, DomainError> Save(string path) {
+        throw new NotImplementedException();
+    }
+    
+    private void InitStorage(string folder) {
+        if (Directory.Exists(folder)) return; // si ya existe sale
+        _logger.Debug("El directorio no existe. Creándolo...");
+        Directory.CreateDirectory(folder); // si no, lo crea
+    }
+}
