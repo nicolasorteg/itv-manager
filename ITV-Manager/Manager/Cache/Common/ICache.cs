e27f125f2cb7c685de69b2e1af45587231de4cc9
nicolasorteg <nicolasortegaf22@gmail.com>
2026-05-22 13:23:03 +0200

    add ICache

---
+++ ITV-Manager/Manager/Cache/Common/ICache.cs
@@ -0,0 +1,19 @@
+﻿namespace Manager.Cache.Common;
+
+/// <summary> Contrato para una caché genérica </summary>
+/// <typeparam name="TKey">Tipo de la clave</typeparam>
+/// <typeparam name="TValue">Tipo del objeto a guardar</typeparam>
+public interface ICache<in TKey, TValue> where TKey : notnull {
+    
+    /// <summary>  Agrega un elemento a la caché. Si está llena, elimina el menos usado. </summary>
+    void Add(TKey key, TValue value);
+    
+    /// <summary> Obtiene un elemento de la caché </summary>
+    TValue? Get(TKey key);
+    
+    /// <summary> Elimina un elemento de la caché </summary>
+    bool Remove(TKey key);
+    
+    /// <summary> Muestra el estado de la caché </summary>
+    void DisplayStatus();
+}
