4dd143b7817d2344905e97e493a0c59e7a4e4762
nicolasorteg <nicolasortegaf22@gmail.com>
2026-05-24 19:48:09 +0200

    end service storage

---
+++ ITV-Manager/Manager/Service/ImportExport/IImportExportService.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFunctionalExtensions;
+using Manager.Errors.Common;
+using Manager.Models;
+
+namespace Manager.Service.ImportExport;
+
+public interface IImportExportService {
+    
+    /// <summary> Importa citas de un archivo </summary>
+    Result<IEnumerable<Cita>, DomainError> ImportarDatos(string path);
+    
+    /// <summary> Exporta citas a un archivo </summary>
+    Result<int, DomainError> ExportarDatos(IEnumerable<Cita> citas, string path);
+
+    /// <summary> Importa personas desde la ruta por defecto </summary>
+    Result<IEnumerable<Cita>, DomainError> ImportarDatosSistema(string path);
+    
+    /// <summary> Exporta citas usando el directorio default </summary>
+    Result<int, DomainError> ExportarDatosSistema(IEnumerable<Cita> citas);
+}
